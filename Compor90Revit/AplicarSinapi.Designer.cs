d48bbedeed9052a0f62fdc854b784de632d55969
charles <charles@PAT-0173>
2017-09-28 15:22:48 -0300

    Add project files.

---
+++ Compor90Revit/AplicarSinapi.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace Compor90Revit
+{
+    partial class AplicarSinapi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AplicarSinapi));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSinapi = new System.Windows.Forms.TextBox();
+            this.btnAplicar = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtProcurarItemCompor = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tvCompor = new System.Windows.Forms.TreeView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.treeElementos = new System.Windows.Forms.TreeView();
+            this.txtProcurarElementoRevit = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 366);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Código de Integração";
+            // 
+            // txtSinapi
+            // 
+            this.txtSinapi.Location = new System.Drawing.Point(122, 365);
+            this.txtSinapi.Margin = new System.Windows.Forms.Padding(2);
+            this.txtSinapi.Name = "txtSinapi";
+            this.txtSinapi.Size = new System.Drawing.Size(675, 20);
+            this.txtSinapi.TabIndex = 2;
+            // 
+            // btnAplicar
+            // 
+            this.btnAplicar.Location = new System.Drawing.Point(801, 365);
+            this.btnAplicar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAplicar.Name = "btnAplicar";
+            this.btnAplicar.Size = new System.Drawing.Size(72, 20);
+            this.btnAplicar.TabIndex = 3;
+            this.btnAplicar.Text = "Aplicar";
+            this.btnAplicar.UseVisualStyleBackColor = true;
+            this.btnAplicar.Click += new System.EventHandler(this.btnAplicar_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(405, 391);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(2);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(56, 23);
+            this.btnOK.TabIndex = 4;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "SINAPI"});
+            this.comboBox1.Location = new System.Drawing.Point(11, 35);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(862, 21);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 17);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Selecione a base de dados";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.txtProcurarItemCompor);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.tvCompor);
+            this.groupBox1.Location = new System.Drawing.Point(11, 62);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(428, 298);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Item Compor 90";
+            // 
+            // button2
+            // 
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(395, 20);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(27, 24);
+            this.button2.TabIndex = 14;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtProcurarItemCompor
+            // 
+            this.txtProcurarItemCompor.Location = new System.Drawing.Point(56, 23);
+            this.txtProcurarItemCompor.Margin = new System.Windows.Forms.Padding(2);
+            this.txtProcurarItemCompor.Name = "txtProcurarItemCompor";
+            this.txtProcurarItemCompor.Size = new System.Drawing.Size(336, 20);
+            this.txtProcurarItemCompor.TabIndex = 11;
+            this.txtProcurarItemCompor.TextChanged += new System.EventHandler(this.txtProcurarItemCompor_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 26);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Procurar";
+            // 
+            // tvCompor
+            // 
+            this.tvCompor.HideSelection = false;
+            this.tvCompor.Location = new System.Drawing.Point(5, 45);
+            this.tvCompor.Margin = new System.Windows.Forms.Padding(2);
+            this.tvCompor.Name = "tvCompor";
+            this.tvCompor.Size = new System.Drawing.Size(419, 245);
+            this.tvCompor.TabIndex = 9;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.treeElementos);
+            this.groupBox2.Controls.Add(this.txtProcurarElementoRevit);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Location = new System.Drawing.Point(445, 62);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(428, 298);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Elemento Revit";
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(395, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(27, 24);
+            this.button1.TabIndex = 13;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // treeElementos
+            // 
+            this.treeElementos.HideSelection = false;
+            this.treeElementos.Location = new System.Drawing.Point(5, 45);
+            this.treeElementos.Margin = new System.Windows.Forms.Padding(2);
+            this.treeElementos.Name = "treeElementos";
+            this.treeElementos.Size = new System.Drawing.Size(418, 245);
+            this.treeElementos.TabIndex = 12;
+            // 
+            // txtProcurarElementoRevit
+            // 
+            this.txtProcurarElementoRevit.Location = new System.Drawing.Point(56, 23);
+            this.txtProcurarElementoRevit.Margin = new System.Windows.Forms.Padding(2);
+            this.txtProcurarElementoRevit.Name = "txtProcurarElementoRevit";
+            this.txtProcurarElementoRevit.Size = new System.Drawing.Size(334, 20);
+            this.txtProcurarElementoRevit.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(5, 26);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Procurar";
+            // 
+            // AplicarSinapi
+            // 
+            this.AcceptButton = this.btnOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(878, 422);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.btnAplicar);
+            this.Controls.Add(this.txtSinapi);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AplicarSinapi";
+            this.ShowInTaskbar = false;
+            this.Text = "Aplicar Integração Compor 90";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSinapi;
+        private System.Windows.Forms.Button btnAplicar;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtProcurarItemCompor;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TreeView tvCompor;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TreeView treeElementos;
+        private System.Windows.Forms.TextBox txtProcurarElementoRevit;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Compor90Revit/AplicarSinapi.cs
@@ -0,0 +1,148 @@
+﻿/////////////////////////////////////////////////////////////////////
+// Copyright (c) Autodesk, Inc. All rights reserved
+// Written by Forge Partner Development
+//
+// Permission to use, copy, modify, and distribute this software in
+// object code form for any purpose and without fee is hereby granted,
+// provided that the above copyright notice appears in all copies and
+// that both that copyright notice and the limited warranty and
+// restricted rights notice below appear in all supporting
+// documentation.
+//
+// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS.
+// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC.
+// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+// UNINTERRUPTED OR ERROR FREE.
+/////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Compor90Revit
+{
+    public partial class AplicarSinapi : Form
+    {
+        public AplicarSinapi()
+        {
+            InitializeComponent();
+        }
+
+        private List<TreeNode> listaEncontrados = new List<TreeNode>();
+        public delegate void AplicarSinapiEvent(int elementId, string sinapi);
+
+        public event AplicarSinapiEvent ElementoSelecionado;
+
+        private void btnAplicar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtSinapi.Text) && tvCompor.SelectedNode == null)
+                MessageBox.Show("Informe o código de integração.");
+
+            string textoParaAplicar = string.Empty;
+            if (tvCompor.SelectedNode != null)
+                textoParaAplicar = tvCompor.SelectedNode.Text.Substring(0, tvCompor.SelectedNode.Text.IndexOf("-") - 1);
+
+            if (!string.IsNullOrEmpty(txtSinapi.Text))
+                textoParaAplicar = txtSinapi.Text;
+
+            if (treeElementos.SelectedNode.Nodes.Count == 0)
+            {
+                ElementoSelecionado.Invoke(Int32.Parse((string)treeElementos.SelectedNode.Tag), textoParaAplicar);
+            }
+            else
+            {
+                foreach (TreeNode node in treeElementos.SelectedNode.Nodes)
+                {
+                    ElementoSelecionado.Invoke(Int32.Parse((string)node.Tag), textoParaAplicar);
+                }
+            }
+
+            MessageBox.Show("Código associado com sucesso.");
+
+        }
+
+        public TreeNodeCollection Tree { get { return treeElementos.Nodes; } }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            System.Windows.Forms.TreeNode treePrincipal = new System.Windows.Forms.TreeNode("SINAPI");
+
+            // carregar o txt referente ao item selecionado
+            // item 0 - SINAP
+            #region SINAPI
+
+            var arquivoSinapi = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData).ToString() + "\\Autodesk\\REVIT\\Addins\\2018\\comp_sinapi.txt";
+
+            string[] lines = System.IO.File.ReadAllLines(arquivoSinapi, Encoding.GetEncoding("iso-8859-1"));
+
+            // Display the file contents to the console. Variable text is a string.
+            //System.Console.WriteLine("Contents of WriteText.txt = {0}", text);
+            foreach (string line in lines)
+            {
+                // Use a tab to indent each line of the file.
+                string texto = line.Substring(0, line.IndexOf("-") - 1).TrimEnd() + " - " + line.Substring(line.IndexOf("-") + 1).Trim();//line.Substring(147, line.Length - 147).TrimEnd() + " - " + line.Substring(23, 61).Trim();
+                System.Windows.Forms.TreeNode tnItem = new System.Windows.Forms.TreeNode(texto);
+                tnItem.Tag = line.Substring(0, line.IndexOf("-") - 1).TrimEnd();
+                treePrincipal.Nodes.Add(tnItem);
+                //Console.WriteLine("\t" + line);
+            }
+
+            tvCompor.Nodes.Add(treePrincipal);
+            tvCompor.Sort();
+            tvCompor.ExpandAll();
+            #endregion
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private TreeNode FindNodeWithContent(TreeNode node, string searchedContent)
+        {
+            if (!listaEncontrados.Contains(node) && node.Text.ToUpper().Contains(searchedContent.ToUpper()))
+                return node;
+
+            foreach (TreeNode childNode in node.Nodes)
+            {
+                TreeNode foundNode = FindNodeWithContent(childNode, searchedContent);
+                if (foundNode != null)
+                    return foundNode;
+            }
+
+            return null;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (tvCompor.Nodes.Count > 0)
+            {
+                tvCompor.SelectedNode = FindNodeWithContent(tvCompor.Nodes[0], txtProcurarItemCompor.Text);
+                listaEncontrados.Add(tvCompor.SelectedNode);
+            }
+            else
+                MessageBox.Show("Informe a base de dados.");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (treeElementos.Nodes.Count > 0)
+                treeElementos.SelectedNode = FindNodeWithContent(treeElementos.Nodes[0], txtProcurarElementoRevit.Text);
+            else
+                MessageBox.Show("Não há elementos para a pesquisa.");
+        }
+
+        private void txtProcurarItemCompor_TextChanged(object sender, EventArgs e)
+        {
+            listaEncontrados.Clear();
+        }
+    }
+}
